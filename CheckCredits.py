3d29d4ab54aa80baddca44aedc2c926c2a2d21f1
Phantom-VK <vikramadityakhupse@gmail.com>
2024-12-22 10:50:17 +0530

    Added Threading for Fast execution Added video duration in csv Can fetch all videos of a channel

--- CheckCredits.py
+++ CheckCredits.py
@@ -7,18 +7,65 @@ def getResponse(api_key, channel_id):
     response = requests.get(playlist_url).json()
     upload_playlist_id = response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
 
-    # Fetch video descriptions
-    video_list_url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={upload_playlist_id}&maxResults=50&key={api_key}"
-    videos = requests.get(video_list_url).json()
+    # Fetch all video descriptions
+    videos = []
+    next_page_token = None
+    while True:
+        # Add contentDetails to get video durations
+        video_list_url = (
+            f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails"
+            f"&playlistId={upload_playlist_id}&maxResults=50&key={api_key}"
+        )
+        if next_page_token:
+            video_list_url += f"&pageToken={next_page_token}"
+
+        response = requests.get(video_list_url).json()
+
+        # Fetch video durations in batch
+        video_ids = [item['contentDetails']['videoId'] for item in response['items']]
+        video_details_url = (
+            f"https://www.googleapis.com/youtube/v3/videos?part=contentDetails"
+            f"&id={','.join(video_ids)}&key={api_key}"
+        )
+        duration_response = requests.get(video_details_url).json()
+
+        # Create duration lookup dictionary
+        duration_dict = {
+            item['id']: item['contentDetails']['duration']
+            for item in duration_response['items']
+        }
+
+        # Add duration to each video item
+        for item in response['items']:
+            video_id = item['contentDetails']['videoId']
+            item['contentDetails']['duration'] = duration_dict.get(video_id, 'N/A')
+
+        videos.extend(response['items'])
+
+        next_page_token = response.get('nextPageToken')
+        if not next_page_token:
+            break
 
     return videos
+
+
+def parse_duration(duration_str):
+    """Convert ISO 8601 duration to minutes"""
+    import re
+    import isodate
+    try:
+        duration = isodate.parse_duration(duration_str)
+        return int(duration.total_seconds() // 60)
+    except:
+        return 0
 
 
 def checkVideos(videos):
     videos_list = []
     channel_name = ""
     index = 1
-    for video in videos['items']:
+
+    for video in videos:
         channel_name = video['snippet']['channelTitle']
         video_details = {}
         description = video['snippet']['description']
@@ -28,12 +75,12 @@ def checkVideos(videos):
         video_id = video['snippet']['resourceId']['videoId']
         video_details["Date"], video_details["Time"] = video['snippet']["publishedAt"].split("T")
         video_details["URL"] = f"https://www.youtube.com/watch?v={video_id}"
+        video_details["Duration"] = parse_duration(video['contentDetails']['duration'])
 
         if "RPM" in description:
             video_details["Credits"] = "FOUND"
         else:
             video_details["Credits"] = "NOT FOUND"
         videos_list.append(video_details)
+
     return videos_list, channel_name
-
-
--- main.py
+++ main.py
@@ -1,19 +1,44 @@
+import threading
 import CheckCredits
 import Config
 import toCSV
 
-CHANNEL_ID_LIST = ["UCiWatT8PZJkw7W8K8j6eGsA", "UCcmLksNmHmp4gZmZCA1-DHA", "UCYpT6g8KlLla4Nyzz6UUtkA",
-                   "UC3v9Y9VNDyVy1TMr0dUoaMQ", "UCySwejnZVKetHGKS9K5B_fg", "UCFrVi-E_Na2syIPK6e6Trbg",
-                   "UCfbTjeLbY49IrOPf3TiwMZQ", "UCA_NIJLe2dNnL4J8M3wrsig"]
+CHANNEL_ID_LIST = [
+    "UCiWatT8PZJkw7W8K8j6eGsA",
+    "UCcmLksNmHmp4gZmZCA1-DHA",
+    "UCYpT6g8KlLla4Nyzz6UUtkA",
+    "UC3v9Y9VNDyVy1TMr0dUoaMQ",
+    "UCySwejnZVKetHGKS9K5B_fg",
+    "UCFrVi-E_Na2syIPK6e6Trbg",
+    "UCfbTjeLbY49IrOPf3TiwMZQ",
+    "UCA_NIJLe2dNnL4J8M3wrsig"
+]
 
-if __name__ == "__main__":
-    print("Program Started")
-    for channel_id in CHANNEL_ID_LIST:
-        videos_list, channel_name = CheckCredits.checkVideos(CheckCredits.getResponse(
+
+def process_channel(channel_id):
+    try:
+        # getResponse now only returns videos, so we don't need to unpack two values
+        videos = CheckCredits.getResponse(
             api_key=Config.API_KEY,
             channel_id=channel_id
         )
-        )
-
+        videos_list, channel_name = CheckCredits.checkVideos(videos)
         toCSV.convertToCSV(videos_list, channel_name)
-    print("Finished!!")
+        print(f"Finished processing channel: {channel_name}")
+    except Exception as e:
+        print(f"Error processing channel {channel_id}: {e}")
+
+
+if __name__ == "__main__":
+    print("Program Started")
+
+    threads = []
+    for channel_id in CHANNEL_ID_LIST:
+        thread = threading.Thread(target=process_channel, args=(channel_id,))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    print("All channels processed. Finished!!")
